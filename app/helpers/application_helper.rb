7a70042a1cc8b29408dd185371ea2bda6b17fccb
Amit Ambardekar <amitamb@gmail.com>
2019-03-20 19:36:46 +0530

    Added admin section and improved navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_link(text, url, method: nil)
+    active_class = nil
+    if current_page?(url)
+      active_class = 'active'
+    end
+    tag.li class: ['nav-item', active_class].compact.join(' ')  do
+      link_to text, url, class: 'nav-link', method: method
+    end
+  end
 end
